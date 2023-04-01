c33428a4f6d66827500c1f527cdf4378419c8833
windowcow <65646883+windowcow@users.noreply.github.com>
2023-04-01 17:06:06 +0900

    Initial Commit

---
+++ Chatudy/ChatudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatudyApp.swift
+//  Chatudy
+//
+//  Created by 강창우 on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct ChatudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chatudy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Chatudy
+//
+//  Created by 강창우 on 2023/04/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
